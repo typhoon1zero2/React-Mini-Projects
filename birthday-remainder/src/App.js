7df2bded5cdcee62ed9a45b1483fa37a40e1ac67
ptnguyen617 <ptnguyen617@yahoo.com>
2022-06-24 16:30:18 +0900

    Created data in page folder

--- birthday-remainder/src/App.js
+++ birthday-remainder/src/App.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import List from "./component/list";
+import Data from "./page/data"
 
 function App() {
   return <h2>reminder project setup</h2>;
--- birthday-remainder/src/page/data.js
+++ birthday-remainder/src/page/data.js
@@ -1,7 +1,37 @@
-import React from 'react'
-
-export default function data() {
-  return (
-    <div>List Components Data</div>
-  )
-}
+export default [
+    {
+      id: 1,
+      name: 'Andy Lau',
+      age: 29,
+      image:
+        'https://res.cloudinary.com/deeztyphoon0529/image/upload/v1655698414/andy_lau_kbrjii.jpg',
+    },
+    {
+      id: 2,
+      name: 'Ruby Lin',
+      age: 32,
+      image:
+        'https://res.cloudinary.com/deeztyphoon0529/image/upload/v1655698423/Ruby-Lin_csadgz.jpg',
+    },
+    {
+      id: 3,
+      name: 'Phong Nguyen',
+      age: 36,
+      image:
+        'https://res.cloudinary.com/deeztyphoon0529/image/upload/v1655697276/profile_yftgmp.jpg',
+    },
+    {
+      id: 4,
+      name: 'Zhang-shi',
+      age: 34,
+      image:
+        'https://res.cloudinary.com/deeztyphoon0529/image/upload/v1655698435/zhang-ziyi_bqhoyb.jpg',
+    },
+    {
+      id: 5,
+      name: 'Spider-Man',
+      age: 29,
+      image:
+        'https://res.cloudinary.com/deeztyphoon0529/image/upload/v1651589927/test/j9cxdisnsbvmmuxlfxiw.jpg',
+    },
+  ];

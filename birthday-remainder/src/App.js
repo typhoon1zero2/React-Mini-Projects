c5ab1064374defd24010b0c1f9665e0cf73ceca6
ptnguyen617 <ptnguyen617@yahoo.com>
2022-06-24 18:50:09 +0900

    Finished project

--- birthday-remainder/src/App.js
+++ birthday-remainder/src/App.js
@@ -3,7 +3,16 @@ import List from "./component/list";
 import Data from "./page/data"
 
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(Data)
+  return (
+    <main>
+      <section className='container'>
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>clear all</button>
+      </section>
+    </main>
+  )
 }
 
 export default App;
--- birthday-remainder/src/component/list.js
+++ birthday-remainder/src/component/list.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from 'react';
 
-export default function list() {
+const List = ({ people }) => {
   return (
-    <div>list Component</div>
-  )
-}
+    <>
+      {people.map((person) => {
+        const { id, name, age, image } = person;
+        return (
+          <article key={id} className='person'>
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years</p>
+            </div>
+          </article>
+        );
+      })}
+    </>
+  );
+};
+
+export default List;

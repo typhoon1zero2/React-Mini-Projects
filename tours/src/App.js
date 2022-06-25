f8571e708c36814e020eedac2c53a99cfc2e9b06
ptnguyen617 <ptnguyen617@yahoo.com>
2022-06-25 09:08:13 +0900

    Setup App js

--- tours/src/App.js
+++ tours/src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import { React,  useState, useEffect } from "react";
 
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return (
-    <h2>Tours App</h2>
-  );
+  return <h2>Tours Project Setup</h2>
 }
 
-export default App;
+export default App

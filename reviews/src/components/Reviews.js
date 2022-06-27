a28134a18aa454002f077e72c71bb3c21c79c1f9
ptnguyen617 <ptnguyen617@yahoo.com>
2022-06-27 21:22:07 +0900

    Added icons

--- reviews/src/components/Reviews.js
+++ reviews/src/components/Reviews.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import people from "../pages/data";
+
 
 function Reviews() {
   return (

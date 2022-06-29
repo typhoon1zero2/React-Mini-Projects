ad567770e89d083467847289a18c85ddc6527a70
ptnguyen617 <ptnguyen617@yahoo.com>
2022-06-29 17:18:48 +0900

    Finalize setup for question app

--- accordion/src/App.js
+++ accordion/src/App.js
@@ -3,7 +3,21 @@ import data from './pages/data';
 import SingleQuestion from './components/Question';
 
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [questions, setQuestions] = useState(data);
+  return (
+    <main>
+      <div className='container'>
+        <h3>questions and answers about login</h3>
+        <section className='info'>
+          {questions.map((question) => {
+            return (
+              <SingleQuestion key={question.id} {...question}></SingleQuestion>
+            );
+          })}
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;

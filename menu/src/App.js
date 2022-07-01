419a917559fb0d51cf47d375f1329da0da9bacdb
ptnguyen617 <ptnguyen617@yahoo.com>
2022-07-01 14:52:16 +0900

    Make it better

--- menu/src/App.js
+++ menu/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import Menu from './component/Menu';
+import Categories from './component/Categories';
+import items from './pages/data';
 
 function App() {
   return <h2>menu project setup</h2>;

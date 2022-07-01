cb83c6ddc7dbb1c5222111ecf5fcfa67af25434d
ptnguyen617 <ptnguyen617@yahoo.com>
2022-07-02 08:56:09 +0900

    Finished function in menu app

--- menu/src/App.js
+++ menu/src/App.js
@@ -5,7 +5,30 @@ import items from './pages/data';
 const allCategories = ['all', ...new Set(items.map((item) => item.category))];
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 }
 
 export default App;

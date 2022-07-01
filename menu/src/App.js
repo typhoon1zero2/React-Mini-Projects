f36d454d5276e7801497a76cdfa57f8285c728fa
ptnguyen617 <ptnguyen617@yahoo.com>
2022-07-02 08:55:19 +0900

    Created all Categories

--- menu/src/App.js
+++ menu/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Menu from './component/Menu';
 import Categories from './component/Categories';
 import items from './pages/data';
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
 
 function App() {
   return <h2>menu project setup</h2>;

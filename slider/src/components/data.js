ca70277fcd357affe46efd435c3a38788e65ed28
ptnguyen617 <ptnguyen617@yahoo.com>
2022-07-04 17:41:29 +0900

    Setup structure

---
+++ slider/src/components/data.js
@@ -0,0 +1,40 @@
+const people = [
+    {
+      id: 1,
+      image:
+        'https://res.cloudinary.com/deeztyphoon0529/image/upload/v1655697276/profile_yftgmp.jpg',
+      name: 'Phong Nguyen',
+      title: 'office manager',
+      quote:
+        'Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.',
+    },
+    {
+      id: 2,
+      image:
+        'https://res.cloudinary.com/deeztyphoon0529/image/upload/v1655698414/andy_lau_kbrjii.jpg',
+      name: 'Andy Lau',
+      title: 'regular guy',
+      quote:
+        'Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.',
+    },
+    {
+      id: 3,
+      image:
+        'https://res.cloudinary.com/deeztyphoon0529/image/upload/v1655698423/Ruby-Lin_csadgz.jpg',
+      name: 'Ruby Lin',
+      title: 'product designer',
+      quote:
+        'Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.',
+    },
+    {
+      id: 4,
+      image:
+        'https://res.cloudinary.com/deeztyphoon0529/image/upload/v1655698435/zhang-ziyi_bqhoyb.jpg',
+      name: 'Zhang Zhi',
+      title: 'the boss',
+      quote:
+        'Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ',
+    },
+  ];
+  
+  export default people;

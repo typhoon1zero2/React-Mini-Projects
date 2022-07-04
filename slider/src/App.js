a955ab35a8fbf50c8ce6a7cdb9fd9dcab59d936d
ptnguyen617 <ptnguyen617@yahoo.com>
2022-07-04 17:42:46 +0900

    make it better

--- slider/src/App.js
+++ slider/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
-import data from './data';
+import data from './components/data';
 function App() {
   return <h2>slider project setup</h2>;
 }

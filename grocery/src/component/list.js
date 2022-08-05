ade39fd304e72dddc20c43011925fe9c5713283f
ptnguyen617 <ptnguyen617@yahoo.com>
2022-08-05 21:39:18 +0900

    Added alert

--- grocery/src/component/list.js
+++ grocery/src/component/list.js
@@ -1,7 +1,13 @@
-import React from 'react'
-import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
-}
+import React, { useEffect } from 'react';
 
-export default List
+const Alert = ({ type, msg, removeAlert, list }) => {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      removeAlert();
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [list]);
+  return <p className={`alert alert-${type}`}>{msg}</p>;
+};
+
+export default Alert;

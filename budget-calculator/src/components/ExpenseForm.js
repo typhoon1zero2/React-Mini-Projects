ee4eede64e8083fab48adf072e1df285dce62e67
ptnguyen617 <ptnguyen617@yahoo.com>
2022-09-22 21:01:02 +0900

    Minor changes

--- budget-calculator/src/components/ExpenseForm.js
+++ budget-calculator/src/components/ExpenseForm.js
@@ -1,3 +1,16 @@
+//rfce
+// import React from 'react'
+
+// function ExpenseForm() {
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+
+// export default ExpenseForm
+/*rfc
 import React from 'react'
 
 export default function ExpenseForm() {
@@ -7,3 +20,57 @@ export default function ExpenseForm() {
     </div>
   )
 }
+*/
+
+import React from "react";
+import { MdSend } from "react-icons/md";
+const ExpenseForm = ({
+  charge,
+  amount,
+  handleCharge,
+  handleAmount,
+  handleSubmit,
+  edit
+}) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="form-center">
+        <div className="form-group">
+          <label htmlFor="expense">charge</label>
+          <input
+            type="text"
+            className="form-control"
+            id="charge"
+            name="charge"
+            placeholder="e.g. rent"
+            value={charge}
+            onChange={handleCharge}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="amount">amount</label>
+          <input
+            type="number"
+            className="form-control"
+            id="amount"
+            name="amount"
+            placeholder="e.g. 100"
+            value={amount}
+            onChange={handleAmount}
+          />
+        </div>
+      </div>
+      <button type="submit" className="btn">
+        {edit ? "edit" : "submit"}
+        {/* submit  */}
+        <MdSend className="btn-icon" />
+      </button>
+    </form>
+  );
+};
+
+export default ExpenseForm;
+
+
+
+

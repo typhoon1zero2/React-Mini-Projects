a2b393b39cb868d880356aabe086ff52bba9d963
ptnguyen617 <ptnguyen617@yahoo.com>
2022-09-26 11:42:11 +0900

    Created and set up handle form and list

--- budget-calculator/src/App.js
+++ budget-calculator/src/App.js
@@ -6,8 +6,34 @@ import Alert from "./components/Alert";
 
 export default function App() {
   return (
-    <div>
-      <h1>test</h1>
-    </div>
-  )
+    <>
+      {alert.show && <Alert type={alert.type} text={alert.text} />}
+      <h1>budget calculator</h1>
+      <main className="App">
+        <ExpenseForm
+          handleSubmit={handleSubmit}
+          charge={charge}
+          handleCharge={handleCharge}
+          amount={amount}
+          handleAmount={handleAmount}
+          edit={edit}
+        />
+        <ExpenseList
+          expenses={expenses}
+          handleDelete={handleDelete}
+          handleEdit={handleEdit}
+          clearItems={clearItems}
+        />
+      </main>
+      <h1>
+        total spending :
+        <span className="total">
+          $
+          {expenses.reduce((acc, curr) => {
+            return (acc += curr.amount);
+          }, 0)}
+        </span>
+      </h1>
+    </>
+  );
 }
